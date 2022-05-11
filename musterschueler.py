8a45b00db172a0fa54bbe4f0fc872ca089fc4b4f
simvalue <simvalue@gmail.com>
2022-05-11 14:31:30 +0200

    Update musterschueler.py

--- musterschueler.py
+++ musterschueler.py
@@ -2,7 +2,7 @@ import math
 #Hier die (erwarteten) Punkte einfüllen
 test_1 = 50 #Erster Test
 test_2 = 20 #Zweiter Test
-shorttest_total = 3 #Alle punkte von kurztests
+shorttest_total = 0 #Alle punkte von kurztests
 num_ue = 11 #10 falls man bei ersten tutorium noch nicht dabei war
 total_ue_tasks = 3*num_ue 
 tasks_done = 17 #Anzahl der erledigten Aufgaben (1 UE = 3 Aufgaben, erledigt bedeutet min 1 punkt) 
@@ -27,8 +27,9 @@ tasks_done_ratio = round(tasks_done/total_ue_tasks, 1)
 if is_berufstaetig:
     total_points = math.floor(200*(test_1+test_2+ue_points_grade)/220 + ue_points_bonus)
     print(f"Punkteformel: P = 200*(T_1+T_2+U_1)/230 + U_2 = 200*({test_1}+{test_2}+{ue_points_grade})/220 + {ue_points_bonus} = {total_points}")
-total_points = math.floor(200*(test_1+test_2+shorttest_grade+ue_points_grade)/230 + shorttest_bonus + ue_points_bonus)
-print(f"Punkteformel: P = 200*(T_1+T_2+K_1+U_1)/230 +K_2 + U_2 = 200*({test_1}+{test_2}+{shorttest_grade}+{ue_points_grade})/230 + {shorttest_bonus} + {ue_points_bonus} = {total_points}")
+else:
+    total_points = math.floor(200*(test_1+test_2+shorttest_grade+ue_points_grade)/230 + shorttest_bonus + ue_points_bonus)
+    print(f"Punkteformel: P = 200*(T_1+T_2+K_1+U_1)/230 +K_2 + U_2 = 200*({test_1}+{test_2}+{shorttest_grade}+{ue_points_grade})/230 + {shorttest_bonus} + {ue_points_bonus} = {total_points}")
 
 passed = True
 if total_points >= 81 :
